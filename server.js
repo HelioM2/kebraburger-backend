e37367067d3ac18d9cb327895201863c7b3084b0
HelioM2 <hantunes059@gmail.com>
2025-04-21 16:06:49 +0100

    update9 connction DB

--- server.js
+++ server.js
@@ -17,35 +17,36 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Conexão à base de dados com variáveis de ambiente
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10, // Número de conexões simultâneas
-    connectTimeout: 10000 
-  })
-  
-  // Testar se conecta
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 10, // Número de conexões simultâneas
+  connectTimeout: 10000
+})
+
+// Testar se conecta
 connection.connect((err) => {
+  if (err) {
+    console.error('Erro na conexão:', err);
+  } else {
+    console.log('Conectado ao MySQL com sucesso!');
+  }
+});
+
+// Enviar um "ping" a cada 5 minutos (300000 ms)
+setInterval(() => {
+  connection.ping(err => {
     if (err) {
-      console.error('Erro na conexão:', err);
+      console.error('Erro ao enviar ping:', err);
     } else {
-      console.log('Conectado ao MySQL com sucesso!');
+      console.log('Ping enviado com sucesso');
     }
   });
+}, 300000); // 5 minutos
 
-  // Enviar um "ping" a cada 5 minutos (300000 ms)
-setInterval(() => {
-    connection.ping(err => {
-      if (err) {
-        console.error('Erro ao enviar ping:', err);
-      } else {
-        console.log('Ping enviado com sucesso');
-      }
-    });
-  }, 300000); // 5 minutos
-  
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+//const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 
