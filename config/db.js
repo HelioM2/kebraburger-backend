84ae8af4834d08adafbcdbbf15652682b4eaf48b
HelioM2 <hantunes059@gmail.com>
2025-04-21 14:40:25 +0100

    update1 connction DB

--- config/db.js
+++ config/db.js
@@ -16,18 +16,12 @@
 
 const mysql = require('mysql2');
 
-// Crie a conexão usando variáveis de ambiente
+// Usando variáveis de ambiente para se conectar ao banco de dados
 const connection = mysql.createConnection({
-
-  DB_HOST: 'mysql.railway.internal',
-  DB_USER: 'root',
-  DB_PASSWORD: 'OqjnltmqGXZoBqaGiFcjKHYuinnCdJah',
-  database: 'kebra_burger',
-
-  host: process.env.DB_HOST, // Substitua pelo nome da variável de ambiente para o host
-  user: process.env.DB_USER, // Substitua pelo nome da variável de ambiente para o usuário
-  password: process.env.DB_PASSWORD, // Substitua pelo nome da variável de ambiente para a senha
-  database: process.env.DB_NAME // Substitua pelo nome da variável de ambiente para o banco de dados
+  host: process.env.DB_HOST, // 'mysql.railway.internal'
+  user: process.env.DB_USER, // 'root'
+  password: process.env.DB_PASSWORD, // Sua senha
+  database: process.env.DB_NAME // 'kebra_burger'
 });
 
 // Conectar ao banco de dados
